9662a5d0cadf1c121da79fd571d915a610e17b6d
Diego Raymundo <diegor_137@outlook.es>
2020-10-13 23:16:38 -0600

    Primer commit

---
+++ Backend/sistema-mc/src/main/java/com/sistema/mc/principal/app/SistemaMcApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.mc.principal.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMcApplication.class, args);
+	}
+
+}
---
+++ Backend/sistema-mc/src/test/java/com/sistema/mc/principal/app/SistemaMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.mc.principal.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
